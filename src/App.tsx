e983450486b4a491aa8a41cd2a629c6a9cca7ae3
AMMAR HAIRI <halusinasibyammar@gmail.com>
2025-08-09 14:40:08 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className=" bg-[url(/background/bg_1.png)] bg-no-repeat bg-cover bg-center w-screen h-screen opacity-50"></div>
+    </>
+  );
+}
+
+export default App;
